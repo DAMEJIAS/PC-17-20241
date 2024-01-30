c28830723286ae2521afa8a66da3b4232f74a2c6
DAMEJIAS <157499664+DAMEJIAS@users.noreply.github.com>
2024-01-30 09:53:40 -0600

    Add files via upload

---
+++ ConsoleApp300124/ConsoleApp300124/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 3;
+int b = a++;
+Console.WriteLine($"a is {a}, b is {b}");
+int c = 3;
+int d = ++c;
+// Prefix means increment c before assigning it.
+Console.WriteLine($"c is {c}, d is {d}");
+int e = 11; 
+int f = 3;
+Console.WriteLine($"e is {e}, f is {f}");
+Console.WriteLine($"e + f = {e + f}");
+Console.WriteLine($"e - f = {e - f}");
+Console.WriteLine($"e * f = {e * f}");
+Console.WriteLine($"e / f = {e / f}");
+Console.WriteLine($"e % f = {e % f}");
+double g = 11.0;
+Console.WriteLine($"g is {g:N1}, f is {f}");
+Console.WriteLine($"g / f = {g / f}");
+int p1 = 6;
+p1 += 3;
+p1 -= 3;
+p1 *= 3;
+p1 /= 3;
+string? authorName = Console.ReadLine();
+int maxLength = authorName?.Length ?? 30;
+authorName ??= "unknown";
+bool p = true; 
+bool q = false;
+Console.WriteLine($"AND | p | q ");
+Console.WriteLine($"P | {p & p,-5} | {p & q,-5} ");
+Console.WriteLine($"q | {q & p,-5} | {q & q,-5} ");
+Console.WriteLine();
+Console.WriteLine($"OR | p | q ");
+Console.WriteLine($"P | {p | p,-5} | {p | q,-5} ");
+Console.WriteLine($"q | {q | p,-5} | {q | q,-5} ");
+Console.WriteLine();
+Console.WriteLine($"xOR | p | q ");
+Console.WriteLine($"P | {p ^ p,-5} | {p ^ q,-5} ");
+Console.WriteLine($"q | {q ^ p,-5} | {q ^ q,-5} ");
+Console.WriteLine(); int x = 10; int y = 6;
+Console.WriteLine($"Expression | Decimal | Binary"); 
+Console.WriteLine($"-_____________________________________");
+
